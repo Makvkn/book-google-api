866f601ce221af690f75f4fb136cf22c688e33e2
maksim makovkin <maksim.makovkin@neuro-city.ru>
2023-03-31 18:29:32 +0300

    31.03.2023

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./Main.css"
+import Book from "../book/Book";
+
+const Main = () => {
+    return (
+        <main className="main">
+            <div className="booksSection">
+
+
+                <div className="booksSection-controlBar">
+                    <h2 className="controlBar_h2">
+                        Found books  :)
+                    </h2>
+                </div>
+                <div className="booksSection_contents">
+                    <ul className="books_list">
+                        <Book />
+                        <Book />
+                        <Book />
+                        <Book />
+                        <Book />
+                        <Book />
+                    </ul>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Main;
